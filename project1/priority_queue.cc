2af55e84f55e5ca3efd0e2d98815d79309bda33c
shinjaelee <49256785+shinjaelee@users.noreply.github.com>
2019-04-17 10:04:57 +0900

    Update priority_queue.cc
    Applying formatting style guildeline. (2 spaces)

--- project1/priority_queue.cc
+++ project1/priority_queue.cc
@@ -10,25 +10,25 @@ templatePriorityQueue<int>::~templatePriorityQueue(){
 
 template <>
 bool templatePriorityQueue<int>::empty() const{
-        return storage.empty();
+  return storage.empty();
 }
 
 template <>
 int templatePriorityQueue<int>::size() {
-        return storage.size();
+  return storage.size();
 }
 
 template <>
 const int& templatePriorityQueue<int>::top() const{
-        return storage.top();
+  return storage.top();
 }
 
 template <>
 void templatePriorityQueue<int>::pop(){
-        storage.pop();
+  storage.pop();
 }
 
 template <>
 void templatePriorityQueue<int>::push(const int& val){
-        storage.push(val);
+  storage.push(val);
 }
